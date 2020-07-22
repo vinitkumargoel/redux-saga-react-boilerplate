4749ec8a97c8296580246d177124d868d003ea4d
Vinit Kumar <vinit928@gmail.com>
2020-07-22 12:01:40 +0530

    added Readme and removed unwanted

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {HashRouter, Route, Switch} from 'react-router-dom'
-import HomePage from "./containers/HomePage/HomePage";
+import HomePage from "./containers/HomePage";
 
 
 const loading = () => <div className="animated fadeIn pt-3 text-center">Loading...</div>
--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {connect} from "react-redux";
+import Header from "../../components/Header";
 
 const HomePage = () => {
     return (
         <div>
-
+            <Header/>
         </div>
     );
 };
